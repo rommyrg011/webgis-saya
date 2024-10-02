a1648bef49d369320519c39e5a730683512b16ef
Zuraida <zuraida@gmail.com>
2024-10-02 13:11:38 +0800

    c

--- qgis.sql
+++ qgis.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 10, 2024 at 02:19 AM
+-- Generation Time: Sep 30, 2024 at 08:00 AM
 -- Server version: 10.1.21-MariaDB
 -- PHP Version: 5.6.30
 
@@ -36,8 +36,8 @@ CREATE TABLE `alat` (
 --
 
 INSERT INTO `alat` (`id_alat`, `nama_alat`) VALUES
-(1, 'Truck'),
-(2, 'Pickup');
+(2, 'Pickup'),
+(4, 'Truck');
 
 -- --------------------------------------------------------
 
@@ -57,7 +57,7 @@ CREATE TABLE `feedback` (
 --
 
 INSERT INTO `feedback` (`id_feedback`, `nama_lengkap`, `tps`, `keterangan`) VALUES
-(10, 'fsdfdsf', 'TPS3R KAYU BULAN', 'dfsdfsdfsdf');
+(3, 'Rommy Gunawan', 'TPS3R KAYU BULAN', 'mentes aja aku ya');
 
 -- --------------------------------------------------------
 
@@ -80,7 +80,26 @@ CREATE TABLE `kecamatan` (
 --
 
 INSERT INTO `kecamatan` (`id_kec`, `nama_kec`, `luas_w`, `penduduk`, `kepadatan`, `lattitude`, `longitude`) VALUES
-(3, 'banjarmasin utara', 'fsfsdf', '100.000', 'fsafsa', '-3.2960704', '114.5995716');
+(486, 'dfsdf', 'gsdgdgds', 'fhfdhdf', 'manusia', 'dsfsdf', 'dsfdsfs'),
+(487, 'cdtrjtfh', 'ggfhjgfjf', 'fdgfdgdf', 'gdfdfgd', 'dfsda', 'eww35232'),
+(488, 'ryry', 'jgfrdr', 'dfgdfwe5', 'fgfgfdg', 'dfgdfgdfg', 'fgdfg'),
+(489, 'rdfwerwer', 'rwerwerwe', 'werwerwe', 'srqwtrey', 'hfhfd', 'fgdgd'),
+(490, 'fdgfdg', 'gffdgd', 'dgdfgdf', 'dgdfgdfg', 'dfgdfgdf', 'dgfgfdgdfg'),
+(491, 'gfdgdf', 'sfsdf', 'ddfdfg', 'fgfdgfd', 'dfgdfas', 'dgdfgdfg'),
+(492, 'fdgfdgfdg', 'dgfdg', 'fdgdf', 'gdfgfd', 'dfgdfadsg', 'fgfdgdf'),
+(493, 'gdfdfgd', 'dfgdfg', 'gdfgdf', 'dfsdf', 'dfdsf', 'fgfgfd'),
+(494, 'fsdsd', 'fgfdgdf', 'dgdgdsg', 'gffdgd', 'dfgdf', 'gfdgd'),
+(498, 'dfgdfgdfgdf', 'dfgfdg', 'dfgfdgdfgdf', 'gdfgeer', 'ggreg', 'dfgfdgdf'),
+(499, 'gdfgfd', 'f', 'dgfdgdf', 'fgfdg', 'fgfdgfd', 'dgdfgd'),
+(500, 'dfgfdg', 'dfgdfg', 'dfgdfg', 'dgdfg', 'gdfgdf', 'dfgdfg'),
+(501, 'dfgddfgdf', 'ertret', 'treter', 'terter', 'tertt', 'etert'),
+(502, 'tretre', 'trtre', 'xzczxczx', 'xcxzc', 'xcxzc', 'dsfdsffsdfds'),
+(503, 'dfdsf', 'dsfsdfsd', 'fsdfsdfsd', 'fsdfsd', 'fsdfsdf', 'sdfsssdf'),
+(504, 'ffsdfsdfsdfsdf', 'dfsdf', 'sdfsd', 'sdfsdf', 'fsd', 'dfdsfsdf'),
+(505, 'fsdfsd', 'dsfsdfsa', 'wrewtertewrew', 'rwerwetretgret', 'eewrwerwe', 'etrytertet'),
+(506, 'dshrdfhfghfg', 'hfhfdhgfdg', 'dfgdfgdfg', 'dfgdfgds', 'sgdgsddfhgdfgdf', 'gdfgdfgdfgdfgdfg'),
+(507, 'fdgdfgdfgdf', 'dfg', 'dgfdfg', 'gdfhfdh', 'fdsgsdfgdf', 'cvgdfgdg'),
+(508, 'fgdfgdsg', 'hfhfdhfd', 'fdhf', 'gfdgdfg', 'dfgfd', 'jgjgh');
 
 -- --------------------------------------------------------
 
@@ -90,6 +109,7 @@ INSERT INTO `kecamatan` (`id_kec`, `nama_kec`, `luas_w`, `penduduk`, `kepadatan`
 
 CREATE TABLE `tps` (
   `id_tps` int(11) NOT NULL,
+  `tipe` varchar(200) NOT NULL,
   `nama_tps` varchar(100) NOT NULL,
   `kecamatan` varchar(100) NOT NULL,
   `alamat` text NOT NULL,
@@ -108,11 +128,8 @@ CREATE TABLE `tps` (
 -- Dumping data for table `tps`
 --
 
-INSERT INTO `tps` (`id_tps`, `nama_tps`, `kecamatan`, `alamat`, `kapasitas`, `ukuran_bangunan`, `jam_operasional`, `alat_angkutan`, `jam_angkutan`, `maps_direction`, `lattitude`, `longitude`, `image`) VALUES
-(15, 'TPS KAYU BULAN', 'BANJARMASIN UTARA', 'Jl. Sungai Andai Komp. Kayu Bulan No.29, Sungai Jingah, Kec. Banjarmasin Utara, Kota Banjarmasin, Kalimantan Selatan 70122', '2000 ton', '10 x 10 m', '24 jam', 'sepeda', '07.00', 'https://feji.us/ccoet3', '-3.2910245', '114.6142293', '801c733f5ea50f63db14dd77b9fa28a2.jpg'),
-(17, 'tps sujing', 'BANJARMASIN UTARA', 'Sultan Adam', '2000 ton', '10 x 10 m', '07.00', 'Truck', '07.00', 'https://feji.us/gklcxh', '-3.2918884', '114.5914696', 'adcd4a3e82b4b90c33cf447292a5ccb4.jpg'),
-(18, 'TPS GARUDA', 'BANJARMASIN BARAT', 'MHJ7+6PV, Jl. Ir. P. Moch. Noor, Pelambuan, Kec. Banjarmasin Bar., Kota Banjarmasin, Kalimantan Selatan 70118', '2000 ton', '10 x 10 m', '07.00', 'truck', '07.00', 'localhost/webgis/login/admin/92430', '-3.3204118', '114.5954241', 'ff03f800a04fe50dc041ac64d1e73870.jpg'),
-(19, 'tps bjm selatan', 'banjarmasin selatan', 'MJ63+928, Depan, Gg. Mangga, Murung Raya, Kec. Banjarmasin Sel., Kota Banjarmasin, Kalimantan Selatan 70236', '2000 ton', '10 x 10 m', '24 jam', 'Truck', '07.00', 'localhost/webgis/login/admin/4259e', '-3.3390993', '114.6020592', '9b5c2f193a46eaa45ed791227daba1c1.jpg');
+INSERT INTO `tps` (`id_tps`, `tipe`, `nama_tps`, `kecamatan`, `alamat`, `kapasitas`, `ukuran_bangunan`, `jam_operasional`, `alat_angkutan`, `jam_angkutan`, `maps_direction`, `lattitude`, `longitude`, `image`) VALUES
+(15, 'TPST3R', 'TPS3R KAYU BULAN', 'BANJARMASIN UTARA', 'Jl. Sungai Andai Komp. Kayu Bulan No.29, Sungai Jingah, Kec. Banjarmasin Utara, Kota Banjarmasin, Kalimantan Selatan 70122', '2000 ton', '10 x 10 m', '24 jam', 'sepeda', '07.00', 'https://www.google.com/maps/dir/-3.353167,114.6186092/Tpst+3R+kayu+bulan/@-3.3176966,114.6030236,13z/data=!4m10!4m9!1m1!4e1!1m5!1m1!1s0x2de423d34ad39915:0x43bc5bbd2d20e827!2m2!1d114.6110909!2d-3.289661!3e0?entry=ttu&g_ep=EgoyMDI0MDkyNC4wIKXMDSoASAFQAw%3D%3D', '-3.2910245', '114.6142293', '5254987564f0e44891fec1b9c615be78.jpg');
 
 -- --------------------------------------------------------
 
@@ -126,13 +143,6 @@ CREATE TABLE `url` (
   `full_url` varchar(1000) NOT NULL,
   `clicks` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `url`
---
-
-INSERT INTO `url` (`id`, `shorten_url`, `full_url`, `clicks`) VALUES
-(33, '4259e', 'https://www.google.com/maps/place/Toko+pakaian+CJa/@-3.3390993,114.6020592,19z/data=!4m15!1m8!3m7!1s0x2de4216ce3bd6ee5:0x19217341e4cf7a7!2sKec.+Banjarmasin+Sel.,+Kota+Banjarmasin,+Kalimantan+Selatan!3b1!8m2!3d-3.333164!4d114.5861171!16s%2Fg%2F121jcd_x!3m5!1s0x2de4216cb412dfdb:0xfe936425cc354a84!8m2!3d-3.3390994!4d114.6025794!16s%2Fg%2F11ng0j1zkw?entry=ttu&g_ep=EgoyMDI0MDkwNC4wIKXMDSoASAFQAw%3D%3D', 0);
 
 -- --------------------------------------------------------
 
@@ -153,7 +163,8 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id_user`, `nama_lengkap`, `username`, `password`, `level`) VALUES
-(1, 'Rommy Gunawan', 'admin', 'admin', 'admin');
+(1, 'Rommy Gunawan', 'admin', 'admin', 'admin'),
+(2, 'Siti Rahmah Zuraida', 'user', 'user', 'admin');
 
 --
 -- Indexes for dumped tables
@@ -203,32 +214,32 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `alat`
 --
 ALTER TABLE `alat`
-  MODIFY `id_alat` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_alat` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT for table `feedback`
 --
 ALTER TABLE `feedback`
-  MODIFY `id_feedback` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id_feedback` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT for table `kecamatan`
 --
 ALTER TABLE `kecamatan`
-  MODIFY `id_kec` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_kec` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=509;
 --
 -- AUTO_INCREMENT for table `tps`
 --
 ALTER TABLE `tps`
-  MODIFY `id_tps` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `id_tps` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 --
 -- AUTO_INCREMENT for table `url`
 --
 ALTER TABLE `url`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
